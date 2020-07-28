66e2b0e2876377592848faff33ca589936c90a93
ydhnwb <yudhanewbie@gmail.com>
2020-07-28 13:10:59 +0700

    Push notif only when logged in

--- app/src/main/java/com/ydhnwb/paperlessapp/utilities/NotificationHelper.kt
+++ app/src/main/java/com/ydhnwb/paperlessapp/utilities/NotificationHelper.kt
@@ -11,21 +11,22 @@ import com.ydhnwb.paperlessapp.ui.main.MainActivity
 object NotificationHelper {
 
     fun displayNotification(context: Context, title: String, body: String, i : Map<String, String>) {
-        val intent = Intent(context, MainActivity::class.java)
-        val pendingIntent = PendingIntent.getActivity(context, 100, intent, PendingIntent.FLAG_CANCEL_CURRENT)
+        if(PaperlessUtil.getToken(context) != null){
+            val intent = Intent(context, MainActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(context, 100, intent, PendingIntent.FLAG_CANCEL_CURRENT)
 
-        val mBuilder = NotificationCompat.Builder(context, MainActivity.CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_stat_name)
-            .setContentTitle(title)
+            val mBuilder = NotificationCompat.Builder(context, MainActivity.CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentTitle(title)
 //            .setContentText(body)
-            .setContentIntent(pendingIntent)
-            .setAutoCancel(true)
-            .setStyle(NotificationCompat.BigTextStyle().bigText(body))
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .setStyle(NotificationCompat.BigTextStyle().bigText(body))
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
 
-        val mNotificationMgr = NotificationManagerCompat.from(context)
-        mNotificationMgr.notify(1, mBuilder.build())
-
+            val mNotificationMgr = NotificationManagerCompat.from(context)
+            mNotificationMgr.notify(1, mBuilder.build())
+        }
     }
 
 }
